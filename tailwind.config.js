494af6f3ec4d275a95e78b54e47be315386482cf
Lottie <155919436+NamenSindEinfach@users.noreply.github.com>
2024-04-23 17:21:55 +0200

    feat: unified colors (rose!)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{njk,js,html,md,yml,yaml}"],
   theme: {
     extend: {
+      colors: {
+        stardrop: {
+          light: colors.rose[100],
+          DEFAULT: colors.rose[200],
+          accent: colors.rose[300],
+        },
+      },
       typography: ({ theme }) => ({
         DEFAULT: {
           css: {
